55a7d103c3759f5cba1f57bd17e6427407090941
BGluth <gluthb@gmail.com>
2016-07-22 16:48:03 -0600

    Added a public utility function to calculate the time remaining until the next sensor needs to be polled
    - Idea would be that this function is called by the library user to determine how long to sleep until the next sensor needs to be polled.

--- source/include/util_functions.h
+++ source/include/util_functions.h
@@ -3,6 +3,7 @@
 
 #include <time.h>
 
+double IoTLib_calculate_time_in_seconds_until_next_sensor_polling();
 time_t _IoTLib_get_current_time();
 
 #endif
--- source/src/util_functions.c
+++ source/src/util_functions.c
@@ -1,7 +1,35 @@
 #include "util_functions.h"
 
+#include <stdint.h>
 #include <stddef.h>
 #include <time.h>
+
+#include "managed_array_definitions.h"
+
+extern struct IoTLib_MngdKVArray_SnsrIDDataPtr IoTLib_getSensorLastPolledTimeFunctions;
+extern struct IoTLib_MngdKVArray_SnsrIDInt IoTLib_sensorPollFrequencies;
+
+double IoTLib_calculate_time_in_seconds_until_next_sensor_polling()
+{
+	time_t rawCurrentTime = _IoTLib_get_current_time();
+	struct tm* currentTime = localtime(&rawCurrentTime);
+	double nearestTimeUntilNextSensorPoll = INTMAX_MAX;
+
+	for (int i = 0; i < IoTLib_getSensorLastPolledTimeFunctions.length; i++)
+	{
+		int currentSensorFrequency = IoTLib_MKV_get(&IoTLib_sensorPollFrequencies,
+			IoTLib_MngdKVArray_SnsrIDInt, IoTLib_getSensorLastPolledTimeFunctions.keys[i]);
+
+		struct tm timeOfCurrentSensorsNextPoll = *currentTime;
+		timeOfCurrentSensorsNextPoll.tm_sec += currentSensorFrequency;
+		double timeUntilCurrentSensorsNextPoll = difftime(mktime(&timeOfCurrentSensorsNextPoll), rawCurrentTime);
+
+		if (timeUntilCurrentSensorsNextPoll < nearestTimeUntilNextSensorPoll)
+			nearestTimeUntilNextSensorPoll = timeUntilCurrentSensorsNextPoll;
+	}
+
+	return nearestTimeUntilNextSensorPoll;
+}
 
 time_t _IoTLib_get_current_time()
 {
